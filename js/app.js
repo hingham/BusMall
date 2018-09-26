e1fc54fe58657efa567a7fee16ace18af4fe7f0f
hingham <hannah.c.ingham@gmail.com>
2018-09-25 17:42:10 -0700

    added graph and css styling

--- js/app.js
+++ js/app.js
@@ -5,37 +5,37 @@ var secondProduct = document.getElementById('image-two');
 var thirdProduct = document.getElementById('image-three');
 var list = document.getElementById('results');
 
+
 var totalVotes = 0;
-var totalImages = []; 
 
 var imgNums = []
 var productArray = [firstProduct, secondProduct, thirdProduct];
+var label = [];
+var clickData = [];
+var clickPercents = [];
+var colorArray = [];
+
+Product.busProducts = [];
 
 function getImgNums(){
     for (var i = 0; i<Product.busProducts.length; i ++){
         imgNums.push(i);
-        console.log(imgNums);
     }
 }
 
 function shuffle(array){
     var i = 0;
     var j = 0;
-    var h = 0;
+    var holder = 0;
 
     for (i=array.length -1; i>0; i-=1){
         j = Math.floor(Math.random() * (i+1));
-        h = array[i];
+        holder = array[i];
         array[i] = array[j];
-        array[j] = h;
+        array[j] = holder;
     }
-    console.log(array);
-
     return imgNums;
 }
-
-
-Product.busProducts = [];
 
 function Product (filepath, productName){
     this.filepath = filepath;
@@ -45,134 +45,211 @@ function Product (filepath, productName){
     Product.busProducts.push(this);
 }
 
-
-new Product('assets/chair.jpg', 'chair');
-new Product('assets/banana.jpg', 'banana');
-new Product('assets/boots.jpg', 'boots');
-new Product('assets/breakfast.jpg', 'breakfast');
-new Product('assets/tauntaun.jpg', 'tauntaum');
-new Product('assets/unicorn.jpg', 'unicorn');
-new Product('assets/usb.gif', 'usb');
-new Product('assets/water-can.jpg', 'water-can');
-new Product('assets/wine-glass.jpg', 'wine-glass');
-
-getImgNums();
-//shuffle(imgNums);
-
-
 function getProducts() {
     for (var i = 0; i< productArray.length; i++){
         productArray[i].src =  Product.busProducts[imgNums[i]].filepath;
         productArray[i].alt = Product.busProducts[imgNums[i]].productName;
-    
-        console.log('filepath: ' + Product.busProducts[imgNums[i]].filepath+
-        'product name: ' + Product.busProducts[imgNums[i]].productName);
-
         Product.busProducts[imgNums[i]].displayedTotal+=1;
 
-        console.log('count: ' + Product.busProducts[imgNums[i]].displayedTotal);
     }
-    console.log('_____________');
 }
 
-    function clickHandler(event) {
 
-        if (!event){
-            shuffle(imgNums);
-            console.log('beginning');
-            getProducts();
-        }
+function getResults(){
+    document.getElementById("products").setAttribute("class", "hidden");
+    getLabels();
+    getClickData();
+    getColors();
+    getClickPercents();
+    getChart();
+    listResults();
+}
 
-        else{
-        var imgName = event.target.alt;
-        console.log('alt: ' + imgName);
-        // for (var i = 0; i<Product.busProducts.length; i++){
-        //     if(Product.busProducts[i].productName===imgName){
-        //         Product.busProducts[i].clickTotal++;
-        //         console.log(Product.busProducts[i].clickTotal++);
-        //         break;
-        //     }
-        // }
+function getVotes () {
+    productDisplay.addEventListener('click', clickHandler);
+}
 
-
-        var itemOne = imgNums.shift();
-        var itemTwo = imgNums.shift();
-        var itemThree = imgNums.shift();
-        console.log('new ' + imgNums);
+//This function calls getResults
+function clickHandler(event) {
+    if (!event){
         shuffle(imgNums);
-        console.log('shuffled ' + imgNums);
-        imgNums.push(itemOne);
-        imgNums.push(itemTwo);
-        imgNums.push(itemThree);
-
         getProducts();
+    }
 
-        totalVotes += 1;
-        console.log('total clicks' + totalVotes);
+    else{
+    var imgName = event.target.alt;
+    for (var i = 0; i<Product.busProducts.length; i++){
+        if(Product.busProducts[i].productName===imgName){
+            Product.busProducts[i].clickTotal++;
+            break;
+        }
+    }
+
+    var itemOne = imgNums.shift();
+    var itemTwo = imgNums.shift();
+    var itemThree = imgNums.shift();
+    shuffle(imgNums);
+    imgNums.push(itemOne);
+    imgNums.push(itemTwo);
+    imgNums.push(itemThree);
+
+    getProducts();
+    totalVotes += 1;
+
+    //actives function that creates chart once max number of clicks reached
+    if (totalVotes === 10){
+        productDisplay.removeEventListener('click', clickHandler);
+        getResults();
+        }
+    }
+}
+
+function getLabels(){
+    for (var i = 0; i< Product.busProducts.length; i++){
+        label.push(Product.busProducts[i].productName);
+    }
+}
+
+function getClickData(){
+    for (var i = 0; i< Product.busProducts.length; i++){
+        clickData.push(Product.busProducts[i].clickTotal);
+    }
+}
+
+function getClickPercents(){
+    for (var i = 0; i< Product.busProducts.length; i++){
+        var percent = 
+        Product.busProducts[i].clickTotal/Product.busProducts[i].displayedTotal;
+        clickPercents.push(Math.floor(percent * 100));
+    }
+}
+
+function getColors () {
+    var colorR=[];
+    var colorG=[];
+    var colorB = [];
+    for (var i = 0; i< Product.busProducts.length; i++){
+    colorR.push(Math.floor(Product.busProducts[i].clickTotal * Math.random() * 200))+10;
+    colorG.push(Math.ceil(Product.busProducts[i].clickTotal * Math.random()* 2)+50);
+    colorB.push(Math.floor(i + 150 * Math.random() )+10);
+    }
     
-        // if (totalVotes === 5){
-        //     productDisplay.removeEventListener('click', clickHandler);
-        //     getResults();
-        // }
+
+    for (var i = 0; i< Product.busProducts.length; i++){
+    colorArray.push('rgba('+colorR[i]+', '+colorG[i]+', '+colorB[i]+', 0.5)');
     }
-    }
+}
 
-clickHandler();
-productDisplay.addEventListener('click', clickHandler);
+function getChart (){
+    var ctx = document.getElementById("chart").getContext('2d');
+    document.getElementById("chart").removeAttribute('class', 'hidden');
+
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+        labels: label,
+        datasets : [{
+            label: 'Votes Per Product ',
+            data:  clickData, 
+             backgroundColor: colorArray,
+        }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        max: 5,
+                        min: 0,
+                        stepSize: 1
+                    }
+                }]
+            }
+        }
+    });
 
 
+    var ctx = document.getElementById("percent-chart").getContext('2d');
+    document.getElementById("percent-chart").removeAttribute('class', 'hidden');
+    var myPercentChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+        labels: label,
+        datasets : [{
+            label: 'Votes Per Products When Displayed (%)',
+            data:  clickPercents, 
+             backgroundColor: colorArray,
+             }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        max: 100,
+                        min: 0,
+                        stepSize: 10
+                    }
+                }]
+            }
+        }
+    });
+}
 
-// function getVotes () {
-//     productDisplay.addEventListener('click', clickHandler);
-// }
+function getProductIntances () {
+    new Product('assets/bag.jpg', 'bag');
+    new Product('assets/chair.jpg', 'chair');
+    new Product('assets/banana.jpg', 'banana');
+    new Product('assets/boots.jpg', 'boots');
+    new Product('assets/bubblegum.jpg', 'bubble');
+    new Product('assets/breakfast.jpg', 'breakfast');
+    new Product('assets/cthulhu.jpg', 'cthulhu');
+    new Product('assets/dog-duck.jpg', 'dog-duck');
+    new Product('assets/dragon.jpg', 'dragon');
+    new Product('assets/pen.jpg', 'pen');
+    new Product('assets/pet-sweep.jpg', 'pet');
+    new Product('assets/scissors.jpg', 'scissors');
+    new Product('assets/shark.jpg', 'shark');
+    new Product('assets/sweep.png', 'sweep  ');
+    new Product('assets/tauntaun.jpg', 'tauntaum');
+    new Product('assets/unicorn.jpg', 'unicorn');
+    new Product('assets/usb.gif', 'usb');
+    new Product('assets/water-can.jpg', 'water-can');
+    new Product('assets/wine-glass.jpg', 'wine-glass');
+}
 
-function getResults() {
+
+function listResults() {
     productDisplay.setAttribute('class', 'hidden');
 
     for(var i = 0; i < Product.busProducts.length; i++){
-        var percent = (Product.busProducts[i].clickTotal/Product.busProducts[i].displayedTotal) * 100;
 
-        var displayedSummary = 'You viewed ' + Product.busProducts[i].productName + ' ' +
-        Product.busProducts[i].displayedTotal + ' times.'
-        var voteSummary = ' You clicked on ' + Product.busProducts[i].productName + ' ' +
-        Product.busProducts[i].clickTotal + ' times.'  
-        var precentSummary = ' You clicked ' + Product.busProducts[i].productName + ' ' + 
-        percent + '% of the time when given the choice.';
+        var summary = Product.busProducts[i].productName + ': ' +
+        Product.busProducts[i].displayedTotal + ' views, ' + Product.busProducts[i].clickTotal + ' votes';
 
         var newLi = document.createElement('li');
-        var liText = document.createTextNode(displayedSummary + voteSummary + precentSummary);
+        var liText = document.createTextNode(summary);
         newLi.appendChild(liText)
         list.appendChild(newLi);
     }
 }
 
 
-
-getVotes();
+getProductIntances();
+getImgNums();
 clickHandler();
+getVotes();
 
 
 
-// //instantiate produts
-// new Product('assets/bag.jpg', 'bag');
-// new Product('assets/chair.jpg', 'chair');
-// new Product('assets/banana.jpg', 'banana');
-// new Product('assets/boots.jpg', 'boots');
-// new Product('assets/bubblegum.jpg', 'bubble');
-// new Product('assets/breakfast.jpg', 'breakfast');
-// new Product('assets/cthulhu.jpg', 'cthulhu');
-// new Product('assets/dog-duck.jpg', 'dog-duck');
-// new Product('assets/dragon.jpg', 'dragon');
-// new Product('assets/pen.jpg', 'pen');
-// new Product('assets/pet-sweep.jpg', 'pet');
-// new Product('assets/scissors.jpg', 'scissors');
-// new Product('assets/shark.jpg', 'shark');
-// new Product('assets/sweep.png', 'sweep  ');
-// new Product('assets/tauntaun.jpg', 'tauntaum');
-// new Product('assets/unicorn.jpg', 'unicorn');
-// new Product('assets/usb.gif', 'usb');
-// new Product('assets/water-can.jpg', 'water-can');
-// new Product('assets/wine-glass.jpg', 'wine-glass');
+
+
+
+
+
+
+
+
+
+
 
 
 
